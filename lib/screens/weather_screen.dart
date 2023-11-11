0e4484f926cad8a5da80e4eda3f3275150c7fa04
m.bour-qaiba <mouhsin.bourqaiba-ext@montreal.ca>
2023-11-11 16:45:05 -0500

    feat - ui weather screen

--- lib/screens/weather_screen.dart
+++ lib/screens/weather_screen.dart
@@ -10,73 +10,68 @@ class WeatherScreen extends StatefulWidget {
 }
 
 class _WeatherScreenState extends State<WeatherScreen> {
-  final txtPlace  = TextEditingController();
+  final txtPlace = TextEditingController();
   Weather result = Weather('', '', 0, 0, 0, 0);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Weather'),),
-      body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: ListView(
-          children: [
-            Padding(padding: EdgeInsets.all(16),
-            child: TextField(
-              controller: txtPlace,
-              decoration: InputDecoration(
-                hintText: 'Enter a city',
-                suffixIcon: IconButton(
-                  icon: Icon(Icons.search),
-                  onPressed: getData,
-                )
-              ),
-            ),
-            ),
-            weatherRow('Place :' , result.name),
-            weatherRow('Description :' , result.description),
-            weatherRow('Temperature :' , result.temperature.toStringAsFixed(2)),
-            weatherRow('Perceived :' , result.perceived.toStringAsFixed(2)),
-            weatherRow('Pressure :' , result.pressure.toString()),
-            weatherRow('Humidity :' , result.humidity.toString()),
-
-
-
-          ],
+        appBar: AppBar(
+          title: Text('Weather'),
         ),
-      )
-    );
+        body: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: ListView(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(16),
+                child: TextField(
+                  controller: txtPlace,
+                  decoration: InputDecoration(
+                      hintText: 'Enter a city',
+                      suffixIcon: IconButton(
+                        icon: Icon(Icons.search),
+                        onPressed: getData,
+                      )),
+                ),
+              ),
+              weatherRow('Place :', result.name),
+              weatherRow('Description :', result.description),
+              weatherRow(
+                  'Temperature :', result.temperature.toStringAsFixed(2)),
+              weatherRow('Perceived :', result.perceived.toStringAsFixed(2)),
+              weatherRow('Pressure :', result.pressure.toString()),
+              weatherRow('Humidity :', result.humidity.toString()),
+            ],
+          ),
+        ));
   }
 
   Future getData() async {
     HttpHelper helper = HttpHelper();
     result = await helper.getWeather(txtPlace.text);
-    setState(() {
-
-    });
+    setState(() {});
   }
 
   Widget weatherRow(String label, String value) {
-    Widget row  =  Padding(
+    Widget row = Padding(
       padding: EdgeInsets.symmetric(vertical: 16),
       child: Row(
         children: [
           Expanded(
               flex: 3,
-              child: Text(label, style: TextStyle(
-                fontSize: 20,
-                color: Theme.of(context).hintColor
-              ),
-              )
-          ),
+              child: Text(
+                label,
+                style:
+                    TextStyle(fontSize: 20, color: Theme.of(context).hintColor),
+              )),
           Expanded(
               flex: 4,
-              child: Text(value, style: TextStyle(
-                  fontSize: 20,
-                  color: Theme.of(context).primaryColor
-              ),
-              )
-          )
+              child: Text(
+                value,
+                style: TextStyle(
+                    fontSize: 20, color: Theme.of(context).primaryColor),
+              ))
         ],
       ),
     );
